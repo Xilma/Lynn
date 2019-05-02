b8bc5a9464ce13a61f9a0f3196b489af7e023cbe
Xilma <atiruo@gmail.com>
2019-05-02 19:13:31 +0300

    Rendered sign up and sign in pages

--- app.py
+++ app.py
@@ -6,11 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
-
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name=name)
+    return render_template('login.html')
 
 if __name__ == '__main__':
 	app.run(debug=True)
