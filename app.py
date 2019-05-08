3576091bcfb77412d583a4fdc7073b87334bebcd
Xilma <atiruo@gmail.com>
2019-05-08 12:51:19 +0300

    solved errors

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ from flask import Flask, render_template
 #create an instance of Flask class
 app = Flask(__name__)
 
-@app.route('/lynn')
+@app.route('/')
 def login():
     return render_template('login.html')
 
-@app.route('/lynn')
+@app.route('/index')
 def index():
     return render_template('index.html')
 
@@ -16,16 +16,16 @@ def index():
 def lynn():
     return render_template('lynn.html')
 
-@app.route('/lynn')
-def lynn():
+@app.route('/friends')
+def friends():
     return render_template('friends.html')
 
-@app.route('/lynn')
-def lynn():
+@app.route('/help_center')
+def help_center():
     return render_template('helpcenter.html')
 
-@app.route('/lynn')
-def lynn():
+@app.route('/settings')
+def settings():
     return render_template('settings.html')
 
 if __name__ == '__main__':
