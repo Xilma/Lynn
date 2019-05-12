968faecddaf4b6c26f05382afe79c278629d56f9
Xilma <atiruo@gmail.com>
2019-05-12 19:13:54 +0300

    Added chat boxes and mockup login

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 #import the Flask class
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 
 #create an instance of Flask class
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('index'))
+        return render_template('login.html', error=error)
     return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
 
 @app.route('/index')
 def index():
@@ -15,6 +26,10 @@ def index():
 @app.route('/lynn')
 def lynn():
     return render_template('lynn.html')
+
+@app.route('/lynn/chat')
+def chatbot():
+    return render_template('chatbot.html')
 
 @app.route('/friends')
 def friends():
